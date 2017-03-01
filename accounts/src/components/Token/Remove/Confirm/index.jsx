ac9465a28c7211fcf81c548d28c4a707f420b1e8
Huong Hk <huong.quoine@gmail.com>
2017-03-01 14:59:54 +0700

    Fix inport TASKS

--- accounts/src/components/Token/Remove/Confirm/index.jsx
+++ accounts/src/components/Token/Remove/Confirm/index.jsx
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux';
-import { TYPES, TASK } from '@quoine/states/confirmations';
+import { TYPES, TASKS } from '@quoine/states/confirmations';
 
 import View from './View';
 
 const mapDispatchToProps = (dispatch) => ({
   onSet: (payload) => dispatch({
     type: TYPES.SET_CONFIRMING,
-    payload: { task: TASK.TOKEN_REMOVE, ...payload },
+    payload: { task: TASKS.TOKEN_REMOVE, ...payload },
   }),
 });
 
