c32e434d2c7de15c258e320f5e9ecce5e2816284
Thien <dvkndn@gmail.com>
2017-03-03 02:05:38 +0900

    Allow vendor id to be set inside resource

--- core/resources/quoine/rest/getHeaders/index.js
+++ core/resources/quoine/rest/getHeaders/index.js
@@ -4,17 +4,25 @@ const base = {
   Accept: 'application/json',
   'X-Quoine-API-Version': 2,
   'X-Quoine-Device': 'web-native',
-  'X-Quoine-Auth': '', // will be set after authentication
+  // will be set in getHeaders
+  'X-Quoine-Auth': '',
+  // might be override in getHeaders
+  // 3 is QUOINE Pte
   'X-Quoine-Vendor-ID': process.env.REACT_APP_VENDOR_ID || 3,
 };
 
-export default ({ path, isForm }) => (
+const getHeaders = ({ path, isForm, vendorId }) => (
   getAuth(path).then((auth) => {
     const headers = new Headers(base);
     headers.set('X-Quoine-Auth', auth);
     if (!isForm) {
       headers.set('Content-Type', 'application/json');
     }
+    if (vendorId) {
+      headers.set('X-Quoine-Vendor-ID', vendorId);
+    }
     return headers;
   })
 );
+
+export default getHeaders;
