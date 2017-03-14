ffb66a53d6c27c53f7876df2df2b33cd4de4e094
Huong Hk <huong.quoine@gmail.com>
2017-03-15 01:28:17 +0700

    Fix checkIsQuoineJapan

--- core/states/user/resources/normalize.js
+++ core/states/user/resources/normalize.js
@@ -1,6 +1,6 @@
 const checkIsQuoineJapan = (vendorId) => (
   process.env.REACT_APP_VENDOR === 'quoine' &&
-  vendorId === process.env.REACT_APP_QUOINEJP_ID
+  Number(vendorId) === Number(process.env.REACT_APP_QUOINEJP_ID)
 );
 
 export default raw => ({
