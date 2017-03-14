e17adcf36efbcc3e2955b11d3df518006a246a75
Huong Hk <huong.quoine@gmail.com>
2017-03-14 17:39:17 +0700

    Clean OrderEntryForm

--- trade/src/components/OrderEntry/Form/index.jsx
+++ trade/src/components/OrderEntry/Form/index.jsx
@@ -1,8 +1,35 @@
-import { withHorizontal } from '@quoine/components/utils';
+import React from 'react';
 
-import Vertical from './Vertical';
-// import Horizontal from './Horizontal';
+import Quantity from 'components/OrderEntry/Form/Quantity';
+import QuantityDisclose from 'components/OrderEntry/Form/QuantityDisclose';
+import Price from 'components/OrderEntry/Form/Price';
+import Leverage from 'components/OrderEntry/Form/Leverage';
+import Direction from 'components/OrderEntry/Form/Direction';
+import Distance from 'components/OrderEntry/Form/Distance';
+import DistanceType from 'components/OrderEntry/Form/DistanceType';
 
-const Horizontal = () => null;
+import styles from './styles.css';
 
-export default withHorizontal({ Vertical, Horizontal });
+const layout = [
+  [Price, Distance, DistanceType],
+  [Quantity, QuantityDisclose],
+  [Leverage, Direction],
+];
+
+const OrderEntryForm = ({ form }) => (
+  <div className={styles.main}>
+    {layout.map((row, i1) => (
+      <div key={i1} className={styles.row}>
+        {row.map((Element, i2) => (
+          <Element key={i2} className={styles.item} form={form} />
+        ))}
+      </div>
+    ))}
+  </div>
+);
+
+OrderEntryForm.propTypes = {
+  form: React.PropTypes.shape({}).isRequired,
+};
+
+export default OrderEntryForm;
