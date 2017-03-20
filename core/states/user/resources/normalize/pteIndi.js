7443bab72c03ff9ceaee6e59a03e7b0a170b186f
Huong Hk <huong.quoine@gmail.com>
2017-03-20 16:41:20 +0700

    Improve normalize to handle user_profile is null

--- core/states/user/resources/normalize/pteIndi.js
+++ core/states/user/resources/normalize/pteIndi.js
@@ -1,3 +1,9 @@
+const getDetail = (raw, key) => (
+  (
+    raw.user_profile && raw.user_profile[key]
+  ) ? raw.user_profile[key] : ''
+);
+
 const pteIndi = (raw) => ({
   pteIndi: {
     name: {
@@ -5,12 +11,12 @@ const pteIndi = (raw) => ({
       last: raw.last_name,
     },
     nationality: raw.nationality,
-    birth: raw.user_profile.birthdate || 0,
+    birth: getDetail(raw, 'birthdate') || 0,
     address: raw.address || '',
-    income: raw.user_profile.other_source_income || '',
-    incomeJob: raw.user_profile.job_title || '',
-    incomeBusinessName: raw.user_profile.business_name || '',
-    incomeBusinessType: raw.user_profile.business_type || '',
+    income: getDetail(raw, 'other_source_income'),
+    incomeJob: getDetail(raw, 'job_title'),
+    incomeBusinessName: getDetail(raw, 'business_name'),
+    incomeBusinessType: getDetail(raw, 'business_type'),
   },
 });
 
