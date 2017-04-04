fe509d55601a53e0f5da72eba2e2a42c72c7f3e7
Huong Hk <huong.quoine@gmail.com>
2017-04-04 13:49:59 +0700

    Handle invalid time

--- core/components/Time/index.jsx
+++ core/components/Time/index.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import styles from './styles.css';
 import formats from './formats';
 
-const Time = ({ value, type }) => (
-  <span className={styles.main}>
-    {value ? formats[type](value * 1000) : '--'}
-  </span>
-);
+const Time = ({ value, type }) => {
+  let result;
+  try {
+    result = formats[type](value * 1000);
+  } catch(e) {
+    result = '--';
+  };
+  return (
+    <span className={styles.main}>{result}</span>
+  );
+};
 
 Time.propTypes = {
   value: React.PropTypes.number.isRequired,
