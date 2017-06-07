eb982e4fcabb8b3c9e5d6733efecb420e4bc82c4
Huong Hk <huong.quoine@gmail.com>
2017-06-07 18:14:09 +0700

    Disable Trading View Chart for Quoine

--- trade/src/components/Charts/Header/View.jsx
+++ trade/src/components/Charts/Header/View.jsx
@@ -3,26 +3,35 @@ import React from 'react';
 import Header from '@quoine/components/Header';
 
 import ToggleDouble from './ToggleDouble';
-import SelectMode from './SelectMode';
+// import SelectMode from './SelectMode';
 
 import styles from './styles.css';
 
-const ChartsHeader = ({ double, onToggleDouble, mode, onSelectMode }) => (
+const ChartsHeader = ({ double, onToggleDouble }) => (
   <Header styleName="hor-small" className={styles.main}>
-    <div className={styles.double}>
-      <ToggleDouble {...{ double, onToggleDouble }} />
-    </div>
-    <div className={styles.mode}>
-      {process.env.REACT_APP_VENDOR === 'qryptos' ? null : (
-        <SelectMode {...{ mode, onSelectMode }} />
-      )}
-    </div>
+    <ToggleDouble {...{ double, onToggleDouble }} />
   </Header>
 );
 
+// TODO:
+// Will be enable when Trading View Chart is ready in QUOINE
+
+// const ChartsHeader = ({ double, onToggleDouble, mode, onSelectMode }) => (
+//   <Header styleName="hor-small" className={styles.main}>
+//     <div className={styles.double}>
+//       <ToggleDouble {...{ double, onToggleDouble }} />
+//     </div>
+//     <div className={styles.mode}>
+//       {process.env.REACT_APP_VENDOR === 'qryptos' ? null : (
+//         <SelectMode {...{ mode, onSelectMode }} />
+//       )}
+//     </div>
+//   </Header>
+// );
+
 ChartsHeader.propTypes = {
   ...ToggleDouble.propTypes,
-  ...SelectMode.propTypes,
+  // ...SelectMode.propTypes,
 };
 
 export default ChartsHeader;
