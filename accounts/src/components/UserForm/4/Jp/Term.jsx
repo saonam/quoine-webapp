f2d1d5ca35a523d207e8f21603b81dd9b8541b55
Pho Huynh <shortgiraffe4@users.noreply.github.com>
2017-06-22 14:31:57 +0700

    Update JFSA documents' urls (#221)

--- accounts/src/components/UserForm/4/Jp/Term.jsx
+++ accounts/src/components/UserForm/4/Jp/Term.jsx
@@ -16,7 +16,7 @@ const UserForm4JpTerm = ({ form, onChange }) => (
 
     <p className={styles.message}>
       当社は
-      <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/7)+e-statement_20170316.pdf" target="_blank" styleName="accent text">
+      <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/0)+e-statement_20170322.pdf" target="_blank" styleName="accent text">
         「書面の電磁的方法による交付等取扱規程」
       </Link>
       に定める通り、利用規約等を電磁的に交付いたします。
@@ -36,31 +36,31 @@ const UserForm4JpTerm = ({ form, onChange }) => (
     <ol className={styles.message}>
       <li>
         1.プライバシーポリシー（個人情報の適切な取扱い及び保護について）
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/1)+privacy_policy_20170316.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/1)+privacy_policy_20170322.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         2.当社サービス
-        <Link pathname="http://s3-ap-northeast-1.amazonaws.com/jfsa/2)%2Bquoine_service_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/2)+quoine_service_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         3.取引ルール
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/3)%2Btrading_rules_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/3)+trading_rules_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         4.利用規約
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/4)%2BT_and_C_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/4)+T_and_C_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         5.リスク説明
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/5)%2Brisks_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/5)+risks_20170424.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         6.特定商取引に関する法律に基づく表示
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/5)+SCTA_20170316.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/6)+SCTA_20170322.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
       <li>
         7.反社会的勢力でないことの確約書
-        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/6)+antisocial_20170316.pdf" target="_blank" styleName="accent text">（PDF）</Link>
+        <Link pathname="https://s3-ap-northeast-1.amazonaws.com/jfsa/7)+antisocial_20170316.pdf" target="_blank" styleName="accent text">（PDF）</Link>
       </li>
     </ol>
 
