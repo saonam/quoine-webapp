1d8c46c9f27009454c31f02c8ffe2b28b31a3a2b
Huong Hk <huong.quoine@gmail.com>
2017-06-28 16:58:06 +0700

    Revert "Prevent clear captcha widget from Sign Up"
    This reverts commit 87c986ae0356645c7f10bb069d041930360bfd35.

--- accounts/src/components/UserForm/4/index.jsx
+++ accounts/src/components/UserForm/4/index.jsx
@@ -4,16 +4,10 @@ import React from 'react';
 import Pte from './Pte';
 import Jp from './Jp';
 
-const UserForm4 = ({ form, ...others }) => (
-  <div>
-    <div style={form.underJFSA ? {} : { display: 'none' }}>
-      <Jp form={form} {...others} />
-    </div>
-    <div style={!form.underJFSA ? {} : { display: 'none' }}>
-      <Pte form={form} {...others} />
-    </div>
-  </div>
-);
+const UserForm4 = ({ form, ...others }) => {
+  const Detail = form.underJFSA ? Jp : Pte;
+  return <Detail form={form} {...others} />;
+};
 
 UserForm4.propTypes = {
   form: PropTypes.shape({
