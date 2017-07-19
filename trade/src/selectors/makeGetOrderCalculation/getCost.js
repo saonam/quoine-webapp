2e430e3bcae245be3fe313ea798234d8f69f00a0
Huong Hk <huong.quoine@gmail.com>
2017-07-19 16:48:59 +0700

    Fix order form logic in Stop & Trailing Stop order (#259)

--- trade/src/selectors/makeGetOrderCalculation/getCost.js
+++ trade/src/selectors/makeGetOrderCalculation/getCost.js
@@ -1,28 +1,15 @@
-// if stop order => reverse side before calculating
-const getSide = (form) => {
-  if (form.type !== 'stop') { return form.side; }
-  return form.side === 'buy' ? 'sell' : 'buy';
-};
-
 const sellCompare = (price, offerPrice) => (price <= offerPrice);
-
 const buyCompare = (price, offerPrice) => (price >= offerPrice);
 
-const getCostByOrderBook = ({ form: orgForm, orderBook }) => {
-  // create form for calculation
-  const form = { ...orgForm };
-  form.side = getSide(form);
+const isBuy = (form) => form.side === 'buy';
+const isSell = (form) => form.side === 'sell';
 
-  // ready ===
-  const isBuy = form.side === 'buy';
+const getBid = (orderBook) => orderBook.buys[0][0];
+const getAsk = (orderBook) => orderBook.sells[0][0];
 
-  // set
-  let price = Number(form.price);
-  if (form.type === 'market') {
-    price = isBuy ? +Infinity : -Infinity;
-  }
-  const book = isBuy ? orderBook.sells : orderBook.buys;
-  const compareFunc = isBuy ? buyCompare : sellCompare;
+const getCostByOrderBook = ({ price, form, orderBook }) => {
+  const book = isBuy(form) ? orderBook.sells : orderBook.buys;
+  const compareFunc = isBuy(form) ? buyCompare : sellCompare;
   let leftQuantity = form.quantity;
   let taker = 0; let maker = 0;
 
@@ -50,4 +37,69 @@ const getCostByOrderBook = ({ form: orgForm, orderBook }) => {
   return { taker, maker };
 };
 
-export default getCostByOrderBook;
+const getCostLimit = ({ form, orderBook }) => {
+  const price = Number(form.price);
+  return getCostByOrderBook({ price, form, orderBook });
+};
+
+const getCostMarket = ({ form, orderBook }) => {
+  const price = isBuy(form) ? +Infinity : -Infinity;
+  return getCostByOrderBook({ price, form, orderBook });
+};
+
+const getCostStop = ({ form, orderBook }) => {
+  const bid = getBid(orderBook);
+  const ask = getAsk(orderBook);
+  const price = Number(form.price);
+  const quantity = Number(form.quantity);
+
+  if (
+    (isBuy(form) && ask < price) ||
+    (isSell(form) && bid > price)
+  ) {
+    return {
+      taker: 0,
+      maker: price * quantity,
+    };
+  }
+
+  return getCostMarket({ form, orderBook });
+};
+
+const getCostTrailingStop = ({ form, orderBook }) => {
+  const bid = getBid(orderBook);
+  const ask = getAsk(orderBook);
+  const quantity = Number(form.quantity);
+
+  let distance = Number(form.distance);
+  if (form.distanceType === 'percentage') {
+    const percent = distance / 100;
+    distance = isBuy(form) ? (ask * percent) : (bid * percent);
+  }
+
+  const price = isBuy(form) ? (ask + distance) : (bid - distance);
+
+  return {
+    taker: 0,
+    maker: price * quantity,
+  };
+};
+
+const funcs = {
+  limit: getCostLimit,
+  market: getCostMarket,
+  stop: getCostStop,
+  'trailing-stop': getCostTrailingStop,
+};
+
+const getCost = ({ form, orderBook }) => {
+  if (!orderBook.buys.length || !orderBook.sells.length) {
+    return { taker: 0, maker: 0 };
+  }
+
+  const func = funcs[form.type];
+
+  return func({ form, orderBook });
+};
+
+export default getCost;
