71ab7627f8a6e812e74e4ae9bb344a712aab7b27
Huong Hk <huong.quoine@gmail.com>
2017-07-25 16:57:57 +0700

    Enable demo mode for staging1

--- accounts/src/components/DemoLink/index.jsx
+++ accounts/src/components/DemoLink/index.jsx
@@ -2,22 +2,40 @@ import React from 'react';
 
 import translate from '@quoine/translate';
 
+import Link from '@quoine/components/Link';
+
 import styles from './styles.css';
 
-const DemoLink = ({ ...others }) => {
-  // Demo only works in sandbox, so this is hardcoded
-  const href = process.env.REACT_APP_VENDOR === 'qryptos' ?
+const isProd = (
+  window.location.origin === 'https://accounts.quoinex.com' ||
+  window.location.origin === 'https://accounts.qryptos.com'
+);
+
+const sandboxUrl = (
+  // Offical demo mode for users will works in sandbox, so this is hardcoded
+  process.env.REACT_APP_VENDOR === 'qryptos' ?
   'https://accounts-sandbox.qryptos.com/demo?continue=https://trade-sandbox.qryptos.com/'
   :
-  'https://accounts-sandbox.quoine.com/demo?continue=https://trade-sandbox.quoine.com/';
+  'https://accounts-sandbox.quoine.com/demo?continue=https://trade-sandbox.quoine.com/'
+);
+
+const DemoLink = ({ ...others }) => {
+  if (isProd) {
+    return (
+      <a
+        href={sandboxUrl}
+        className={styles.link}
+        {...others}
+      >
+        {translate('demo:title')}
+      </a>
+    );
+  }
+
   return (
-    <a
-      href={href}
-      className={styles.link}
-      {...others}
-    >
+    <Link pathname="/demo" styleName="accent text" {...others}>
       {translate('demo:title')}
-    </a>
+    </Link>
   );
 };
 
