ab95e716a2de98d8bc0218a62f4cf2541a51e4dc
Huong Hk <huong.quoine@gmail.com>
2017-07-26 10:44:24 +0700

    Fix bug continue to show tag in other screen after user finish sign up

--- accounts/src/components/SignUp/onSuccess.js
+++ accounts/src/components/SignUp/onSuccess.js
@@ -1,18 +1,63 @@
+const removeElement = (id) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.parentNode.removeChild(element);
+  }
+};
+
+const appendImg = (imgSrcs) => {
+  imgSrcs.forEach((src, index) => {
+    // generate id
+    const id = `affliate-tag-img-${index}`;
+
+    // remove if exist
+    removeElement(id);
+
+    // create affilate tag
+    const tag = new Image(1, 1);
+    tag.id = id;
+    tag.src = src;
+    tag.style.display = 'none';
+
+    // append the tag to body
+    document.body.appendChild(tag);
+  });
+};
+
+const appendScript = (innerHTML) => {
+  // generate id
+  const scriptId = 'affiliate-tag-script';
+
+  // remove if exist
+  removeElement(scriptId);
+
+  // create affilate tag
+  const script = document.createElement('script');
+  script.id = scriptId;
+  script.type = 'text/javascript';
+  script.innerHTML = innerHTML;
+
+  // append the tag to body
+  document.body.appendChild(script);
+};
+
 const onSuccess = (userId) => {
   if (process.env.REACT_APP_VENDOR === 'traders') {
     // For TRADERS
-    const image = new Image(1, 1);
-    image.src = `https://admin.mtrf.net/ac/action.php?cid=AD000001&uid=${userId}&pid=1`;
-    image.style.display = 'none';
-    document.body.appendChild(image);
+    const imgSrcs = [
+      `https://admin.mtrf.net/ac/action.php?cid=AD000153&uid=${userId}&pid=1`,
+      `https://action.j-a-net.jp///${userId}/405949`,
+      `https://www.tcs-asp.net/aresult?LC=MBTC1&NK=${userId}`,
+      `https://is.accesstrade.net/cgi-bin/isatV2/min-btc/isatWeaselV2.cgi?result_id=100&verify=${userId}`,
+      `https://fx-on.com/rc.php?t=0&m=0&e=${userId}`,
+    ];
+    appendImg(imgSrcs);
   } else {
     // For QUOINE
     const clientId = '7F4H6';
 
     // add script
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerHTML = `
+    appendScript(`
       /* <![CDATA[ */
       var hc_access_key = "${clientId}";
       var hc_unique_id = ${userId};
@@ -26,32 +71,16 @@ const onSuccess = (userId) => {
       hc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 't.82comb.net/tk/conversion.js';
       var ps = document.getElementsByTagName('script')[0]; ps.parentNode.insertBefore(hc, ps);
       })();
-    `;
-    document.body.appendChild(script);
+    `);
 
-    // add main image
-    const main = new Image(1, 1);
-    main.src = `//t.82comb.net/cv?t=img&guid=ON&ak=${clientId}&uq=${userId}&ec=`;
-    main.style.display = 'none';
-    document.body.appendChild(main);
-
-    // tag for mfro
-    const mfro = new Image(1, 1);
-    mfro.src = `https://admin.mtrf.net/ac/action.php?cid=AD000158&uid=${userId}&pid=1`;
-    mfro.style.display = 'none';
-    document.body.appendChild(mfro);
-
-    // tag for JAnat
-    const JAnat = new Image(1, 1);
-    JAnat.src = `https://action.j-a-net.jp///${userId}/405926`;
-    JAnat.style.display = 'none';
-    document.body.appendChild(JAnat);
-
-    // tag for A8
-    const A8 = new Image(1, 1);
-    A8.src = `https://px.a8.net/cgi-bin/a8fly/sales?pid=s00000017973001&so=${userId}&si=1.1.1.a8`;
-    A8.style.display = 'none';
-    document.body.appendChild(A8);
+    // add images
+    const imgSrcs = [
+      `//t.82comb.net/cv?t=img&guid=ON&ak=${clientId}&uq=${userId}&ec=`,
+      `https://admin.mtrf.net/ac/action.php?cid=AD000158&uid=${userId}&pid=1`,
+      `https://action.j-a-net.jp/${userId}/405926`,
+      `https://px.a8.net/cgi-bin/a8fly/sales?pid=s00000017973001&so=${userId}&si=1.1.1.a8`,
+    ];
+    appendImg(imgSrcs);
   }
 };
 
