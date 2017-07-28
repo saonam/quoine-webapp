160ec759c08e6057240668d3573799c02cf97831
Huong Hk <huong.quoine@gmail.com>
2017-07-28 12:01:41 +0700

    Fix bug don't reload crypto address

--- trade/src/components/WdrAddress/Wrapper.jsx
+++ trade/src/components/WdrAddress/Wrapper.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import Body from './Body';
 
 class WdrAddress extends React.PureComponent {
+  componentDidMount() {
+    this.props.onAccountChange(this.props.account);
+  }
   componentDidUpdate(prevProps) {
     if (this.props.account !== prevProps.account) {
       this.props.onAccountChange(this.props.account);
