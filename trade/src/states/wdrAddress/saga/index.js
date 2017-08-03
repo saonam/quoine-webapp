2f112fbc9b58f0b56be63ce280cf7a03c0a1c600
Huong Hk <huong.quoine@gmail.com>
2017-08-03 15:08:36 +0700

    Clean

--- trade/src/states/wdrAddress/saga/index.js
+++ trade/src/states/wdrAddress/saga/index.js
@@ -3,7 +3,6 @@ import { spawn } from 'redux-saga/effects';
 import init from './init';
 import watchAccountChange from './watchAccountChange';
 import watchAdd from './watchAdd';
-import watchEdit from './watchEdit';
 import watchRemove from './watchRemove';
 import watchVerify from './watchVerify';
 import watchDismiss from './watchDismiss';
@@ -13,7 +12,6 @@ export default function* wdrAddressSaga() {
     spawn(init),
     spawn(watchAccountChange),
     spawn(watchAdd),
-    spawn(watchEdit),
     spawn(watchRemove),
     spawn(watchVerify),
     spawn(watchDismiss),
