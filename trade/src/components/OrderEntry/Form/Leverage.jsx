14fa26c8cf0ef22ba805dd958b1ed940684c1a27
Tam Pham <tampham47@live.com>
2017-08-11 17:03:56 +0700

    Allow 50X Leverage for Corporate Customers (#261)

--- trade/src/components/OrderEntry/Form/Leverage.jsx
+++ trade/src/components/OrderEntry/Form/Leverage.jsx
@@ -6,24 +6,42 @@ import React from 'react';
 import SelectInput from '@quoine/components/SelectInput';
 import Field from './Field';
 
-const options = [2, 4, 5, 10, 25].map(level => ({
+
+let options = [2, 4, 5, 10, 25].map(level => ({
+  value: level, label: `${level}x`,
+}));
+const optionsCorpPTE = [2, 4, 5, 10, 25, 50].map(level => ({
   value: level, label: `${level}x`,
 }));
 
-const OrderEntryFormLeverage = ({ form, onChange }) => (
-  <Field
-    id="leverage"
-    hintStyle={{ width: 200, left: -50 }}
-    // modalContent={<ModalContent />}
-  >
-    <SelectInput
-      onChange={onChange} value={form.leverage}
-      options={options} border
-    />
-  </Field>
-);
+const OrderEntryFormLeverage = ({ form, onChange, user }) => {
+  // allow CORP PTE users trade with a leverage of 50
+  // only apply for Quoinex
+  if (
+    process.env.REACT_APP_VENDOR === 'quoine' &&
+    !user.underJFSA && user.type === 'corporate'
+  ) {
+    options = optionsCorpPTE;
+  }
+
+  return (
+    <Field
+      id="leverage"
+      hintStyle={{ width: 200, left: -50 }}
+    >
+      <SelectInput
+        onChange={onChange} value={form.leverage}
+        options={options} border
+      />
+    </Field>
+  );
+};
 
 OrderEntryFormLeverage.propTypes = {
+  user: PropTypes.shape({
+    underJFSA: PropTypes.bool.isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
   form: PropTypes.shape({
     leverage: PropTypes.number.isRequired,
     market: PropTypes.string.isRequired,
