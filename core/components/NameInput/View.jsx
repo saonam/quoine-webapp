6ec71326b9685d0fdddae777e3d77a10e9415530
Huong Hk <huong.quoine@gmail.com>
2017-08-17 11:28:47 +0700

    Switch order of First Name and Last Name for Sign Up JFSA (#271)

--- core/components/NameInput/View.jsx
+++ core/components/NameInput/View.jsx
@@ -8,26 +8,40 @@ import TextInput from '@quoine/components/TextInput';
 import styles from './styles.css';
 
 const NameInputView = ({
-  value, onChange, autoFocus, firstLabel, lastLabel, ...others
-}) => (
-  <div className={styles.main}>
-    <div className={styles.input}>
-      <TextInput
-        {...others}
-        value={value.first} onChange={onChange.first}
-        placeholder={firstLabel || translate('name:first')}
-        autoFocus={autoFocus}
-      />
+  value, onChange, autoFocus, firstLabel, lastLabel, reverse, ...others
+}) => {
+  const first = (
+    <TextInput
+      {...others}
+      value={value.first} onChange={onChange.first}
+      placeholder={firstLabel || translate('name:first')}
+      autoFocus={autoFocus}
+    />
+  );
+  const last = (
+    <TextInput
+      {...others}
+      value={value.last} onChange={onChange.last}
+      placeholder={lastLabel || translate('name:last')}
+    />
+  );
+
+  if (reverse) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.input}>{last}</div>
+        <div className={styles.input}>{first}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.input}>{first}</div>
+      <div className={styles.input}>{last}</div>
     </div>
-    <div className={styles.input}>
-      <TextInput
-        {...others}
-        value={value.last} onChange={onChange.last}
-        placeholder={lastLabel || translate('name:last')}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 NameInputView.propTypes = {
   value: PropTypes.shape({}).isRequired,
@@ -35,6 +49,7 @@ NameInputView.propTypes = {
   autoFocus: PropTypes.bool,
   firstLabel: PropTypes.string,
   lastLabel: PropTypes.string,
+  reverse: PropTypes.bool,
 };
 
 export default NameInputView;
